cc1ee3b4d02e9dfcfb44d1431dab90b0e713fe8b
Shovon15 <shovonrnbd@gmail.com>
2023-08-28 15:49:45 +0600

    .

--- src/secret.js
+++ src/secret.js
@@ -1,7 +1,8 @@
 require("dotenv").config();
 
 const serverPort = process.env.SERVER_PORT || 5001;
-const mongoDB = process.env.MONGODB_ATLAS_URL;
+const mongoDB =
+	process.env.MONGODB_ATLAS_URL || "mongodb+srv://shovon:n9FrwkcKhla2d6t4@cluster0.o5a9jb9.mongodb.net/airbnbDB";
 
 module.exports = {
 	serverPort,
